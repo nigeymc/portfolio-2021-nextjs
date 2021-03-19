537b32d0b10828a20ef73773035cc1c20518eed8
nigeymc <mckenna.niall@gmail.com>
2021-03-19 19:10:14 +0000

    sass issues

--- next.config.js
+++ next.config.js
@@ -16,6 +16,6 @@ module.exports = {
         return config
     },
     sassOptions: {
-        includePaths: [path.resolve(__dirname, 'node_modules')],
+        includePaths: [path.join(__dirname, 'styles')],
     }
 }
