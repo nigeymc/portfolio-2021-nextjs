7cee880c3ed63020b6ff1a358a3f87df82082fd7
nigeymc <mckenna.niall@gmail.com>
2021-03-20 20:45:28 +0000

    latest

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/_Global.scss';
-import '../styles/variables/_Colors.scss';
-import '../styles/variables/_Typography.scss';
-import '../styles/variables/_Spacing.scss';
-import '../styles/variables/_Grid.scss';
+import '../styles/Global.scss';
+import '../styles/variables/Colors.scss';
+import '../styles/variables/Typography.scss';
+import '../styles/variables/Spacing.scss';
+import '../styles/variables/Grid.scss';
 import '../styles/styles.scss';
-import "../styles/sections/_Homepage-section.scss";
-import "../styles/sections/_General-section.scss";
+import "../styles/sections/Homepage-section.scss";
+import "../styles/sections/General-section.scss";
 import { Provider } from 'react-redux';
 import { useStore } from '../store/configureStore';
 
