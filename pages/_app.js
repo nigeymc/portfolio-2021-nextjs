71ff2234fed31efdcc0c26a9a5f1c231cbb284ff
nigeymc <mckenna.niall@gmail.com>
2021-03-22 10:05:00 +0000

    refactored Scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Global.scss';
-import '../styles/variables/Colors.scss';
-import '../styles/variables/Typography.scss';
-import '../styles/variables/Spacing.scss';
-import '../styles/variables/Grid.scss';
-import '../styles/styles.scss';
-import "../styles/sections/Homepage-section.scss";
-import "../styles/sections/General-section.scss";
 import { Provider } from 'react-redux';
 import { useStore } from '../store/configureStore';
 
