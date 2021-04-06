8fcb3e7dffa95a252311c96cf2a1ae990f8d3e3d
nigeymc <mckenna.niall@gmail.com>
2021-04-06 17:15:47 +0100

    improving page load speed

--- pages/_document.js
+++ pages/_document.js
@@ -11,14 +11,17 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta name="description"
-                        content="The personal website of London based Front End Engineer and designer Niall McKenna.I design and build websites and web applications using Javascript, ReactJS and HTML." />
+                        content="London based Front End Engineer and designer Niall McKenna - This is my personal website. I design and build websites and web applications using the Javascript, ReactJS, HTML CSS/SaSS stack." />
                     <meta name="Keywords"
-                        content="front end, engineer, developer, designer, web designer, web developer, javascript, reactJS, html, css, sass, coder" />
+                        content="front end, engineer, developer, designer, web designer, web developer, javascript, reactJS, html, css, SaSS, coder, Wordpress" />
                     <meta name="author" content="Niall McKenna" />
                     <meta name="GOOGLEBOT" content="ALL, NOARCHIVE" />
                     <meta name="SLURP" content="NOARCHIVE" />
                     <meta name="MSNBOT" content="NOARCHIVE" />
                     <meta name="robots" content="index, follow" />
+                    <link defer href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+                    <link defer href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+                    <link defer href="https://fonts.googleapis.com/css2?family=Love+Ya+Like+A+Sister&display=swap" rel="stylesheet" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon_io/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon_io/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon_io/favicon-16x16.png" />
@@ -27,9 +30,6 @@ class MyDocument extends Document {
                 <body>
                     <Main />
                     <NextScript />
-                    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
-                    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
-                    <link href="https://fonts.googleapis.com/css2?family=Love+Ya+Like+A+Sister&display=swap" rel="stylesheet" />
                     <script defer src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossOrigin="anonymous"></script>
                     <script defer src="https://code.responsivevoice.org/responsivevoice.js?key=j9X4oczw"></script>
                 </body>
