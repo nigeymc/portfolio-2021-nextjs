388aaa07a6f968db9b65c0ecd5945f6c8e40bf7c
nigeymc <mckenna.niall@gmail.com>
2021-04-06 17:24:25 +0100

    latest changes

--- pages/_document.js
+++ pages/_document.js
@@ -19,9 +19,6 @@ class MyDocument extends Document {
                     <meta name="SLURP" content="NOARCHIVE" />
                     <meta name="MSNBOT" content="NOARCHIVE" />
                     <meta name="robots" content="index, follow" />
-                    <link defer href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
-                    <link defer href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
-                    <link defer href="https://fonts.googleapis.com/css2?family=Love+Ya+Like+A+Sister&display=swap" rel="stylesheet" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon_io/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon_io/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon_io/favicon-16x16.png" />
@@ -32,6 +29,9 @@ class MyDocument extends Document {
                     <NextScript />
                     <script defer src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossOrigin="anonymous"></script>
                     <script defer src="https://code.responsivevoice.org/responsivevoice.js?key=j9X4oczw"></script>
+                    <link defer href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+                    <link defer href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+                    <link defer href="https://fonts.googleapis.com/css2?family=Love+Ya+Like+A+Sister&display=swap" rel="stylesheet" type="text/css" />
                 </body>
             </Html>
         )
