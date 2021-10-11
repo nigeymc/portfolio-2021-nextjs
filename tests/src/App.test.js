17867225685389772d0d4677bc9bceb842e0510e
nigeymc <mckenna.niall@gmail.com>
2021-10-11 17:15:25 +0100

    testing libraries removed for now:

--- tests/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { shallow } from "enzyme";
-import renderer from "react-test-renderer";
-import App from "../../pages/index";
-
-it("renders without crashing", () => {
-    shallow(<App />);
-});
-
--- tests/src/components/Layout.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { Layout } from '../../../components/Layout';
-
-test('should render Layout correctly', () => {
-    const wrapper = shallow(<Layout content={children} />);
-    expect(wrapper).toMatchSnapshot();
-});
