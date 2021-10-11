019ad466f8b6d0ea3118501fb84b87375f0c40ab
nigeymc <mckenna.niall@gmail.com>
2021-10-11 17:45:56 +0100

    firebase update for app check 5

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,6 +1,6 @@
 // import * as firebase from 'firebase';
 import firebase from 'firebase/app'
-import { initializeAppCheck, ReCaptchaV3Provider } from ('firebase/app-check');
+import 'firebase/appCheck'
 
 import 'firebase/auth'
 import 'firebase/database'
@@ -17,13 +17,15 @@ const config = {
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
 
-  const appCheck = initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider('abcdefghijklmnopqrstuvwxy-1234567890abcd'),
+  const appCheck = firebase.appCheck();
+  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+  // key is the counterpart to the secret key you set in the Firebase console.
+  appCheck.activate(
+    'abcdefghijklmnopqrstuvwxy-1234567890abcd',
 
     // Optional argument. If true, the SDK automatically refreshes App Check
     // tokens as needed.
-    isTokenAutoRefreshEnabled: true
-  });
+    true);
 }
 
 const database = firebase.database();
