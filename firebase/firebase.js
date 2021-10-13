41a5aaa7baeb4c0d8a538dbbb5631ee52726ffd5
nigeymc <mckenna.niall@gmail.com>
2021-10-13 15:22:03 +0100

    firebase update for app check 12

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,5 +1,5 @@
-import * as firebase from 'firebase';
-// import firebase from 'firebase/app'
+// import * as firebase from 'firebase';
+import firebase from 'firebase/app'
 
 import 'firebase/auth'
 import 'firebase/database'
@@ -16,15 +16,15 @@ const config = {
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
 
-  const appCheck = firebase.appCheck();
-  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-  // key is the counterpart to the secret key you set in the Firebase console.
-  appCheck.activate(
-    process.env.GOOGLE_APP_CHECK_SITE_KEY,
+  // const appCheck = firebase.appCheck();
+  // // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+  // // key is the counterpart to the secret key you set in the Firebase console.
+  // appCheck.activate(
+  //   process.env.GOOGLE_APP_CHECK_SITE_KEY,
 
-    // Optional argument. If true, the SDK automatically refreshes App Check
-    // tokens as needed.
-    true);
+  //   // Optional argument. If true, the SDK automatically refreshes App Check
+  //   // tokens as needed.
+  //   true);
 }
 
 const database = firebase.database();
